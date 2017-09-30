6004c70cc9162e0d5ee400658481c9a65f974a6b
hadesyi <leesk55@gmail.com>
2017-09-30 22:11:19 +0900

    hello 애플리케이션 작성하기

---
+++ src/hello.kt
@@ -0,0 +1,15 @@
+/**
+ * Created by hadeslee on 2017-09-30.
+ */
+
+fun main(args: Array<String>) {
+    printHello(1)
+}
+
+fun printHello(msgType: Int) {
+    when (msgType) {
+        1, 2 -> println("안녕하세요?")
+        else -> println("메시지 번호 오류")
+
+    }
+}
