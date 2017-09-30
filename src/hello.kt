900b293da5a2a32ec42a21ba9072e2eed1ea658d
hadesyi <leesk55@gmail.com>
2017-09-30 22:12:16 +0900

    hello 애플리케이션 작성하기

--- src/hello.kt
+++ src/hello.kt
@@ -3,13 +3,8 @@
  */
 
 fun main(args: Array<String>) {
-    printHello(1)
+    print("안녕하세요")
 }
 
-fun printHello(msgType: Int) {
-    when (msgType) {
-        1, 2 -> println("안녕하세요?")
-        else -> println("메시지 번호 오류")
 
-    }
-}
+
