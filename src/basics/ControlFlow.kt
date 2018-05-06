1f4e8f9a3fadabbaa85de6773e093748bbd0d5b8
hadesyi <leesk55@gmail.com>
2018-05-07 06:31:09 +0900

    When Expression

--- src/basics/ControlFlow.kt
+++ src/basics/ControlFlow.kt
@@ -22,4 +22,12 @@ fun main(args: Array<String>) {
         print("Choose b")
         b
     }
+
+    when (x) {
+        1 -> print("x == 1")
+        2 -> print("x == 2")
+        else -> { // Note the block
+            print("x is neither 1 nor 2")
+        }
+    }
 }
