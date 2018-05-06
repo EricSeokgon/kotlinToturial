ca469ba1923cf244e42cb9f95cf5e3b46f90b96d
hadesyi <leesk55@gmail.com>
2018-05-07 06:30:08 +0900

    If Expression

---
+++ src/basics/ControlFlow.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    // Traditional usage
+    var max = a
+    if (a < b) max = b
+
+// With else
+    var max: Int
+    if (a > b) {
+        max = a
+    } else {
+        max = b
+    }
+
+// As expression
+    val max = if (a > b) a else b
+}
